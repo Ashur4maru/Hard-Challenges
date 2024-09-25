c35784f6d3f80c33aa7c1e124488d96832945d50
Ashur4maru <nightemere01@gmail.com>
2024-09-25 23:40:15 +0200

    klaar

--- oefening1.mjs
+++ oefening1.mjs
@@ -2,18 +2,9 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
-let aantalDagen = parseFloat(await userInput.question('Geef een aantal dagen: '));
-let aantalUren = parseFloat(await userInput.question('Geef een aantal uren: '));
-let aantalMinuten = parseFloat(await userInput.question('Geef een aantal minuten: '));
-let aantalSeconden = parseFloat(await userInput.question('Geef een aantal seconde: '));
+let dag = await userInput.question('Welke dag van de week is het vandaag? ');
+let maand = await userInput.question('Welke maand zijn we vandaag? ');
+let dagmaand = await userInput.question('De hoeveelste dag van de maand is het? ');
 
-let dagenInSeconde = (((aantalDagen * 24) * 60) * 60);
-let urenInSeconde = ((aantalUren * 60) * 60);
-let minutenInSeconde = aantalMinuten * 60;
-
-
-
-console.log('Aantal seconden: ' + (dagenInSeconde + urenInSeconde + minutenInSeconde + aantalSeconden));
-
+console.log('Het is vandaag' + dag, dagmaand, maand);
 process.exit();
-
